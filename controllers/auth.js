081799e7691e5aa2f090fd0d7e42ba3c5d45d3c2
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-06-23 15:33:20 -0600

    save usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,20 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body;
-
-  res.status(201).json({
-    ok: true,
-    msg: "REGISTER",
-    name,
-    email,
-    password,
-  });
+const crearUsuario = async (req, res = response) => {
+  try {
+    const usuario = new Usuario(req.body);
+    await usuario.save();
+    res.status(201).json({
+      ok: true,
+      msg: "Usuario registrado!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Error al registro del usuario.",
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
