92d86346fbb9723e4199b7ee2defdff827f5317e
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-06-28 16:18:08 -0600

    Adding events validatiosn and apis

---
+++ controllers/events.js
@@ -0,0 +1,38 @@
+const { response } = require("express");
+
+const getEvents = async (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "getEventos",
+  });
+};
+
+const createEvent = async (req, res = response) => {
+  console.log(req.body);
+
+  res.json({
+    ok: true,
+    msg: "createEvent",
+  });
+};
+
+const updateEvent = async (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "updateEvent",
+  });
+};
+
+const deleteEvent = async (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "deleteEvent",
+  });
+};
+
+module.exports = {
+  getEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+};
--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,43 @@
-const router = require("./auth");
+/*
+    Rutas de usuarios /events
+    host + /api/events
+*/
+const { Router } = require("express");
+const {
+  getEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+} = require("../controllers/events");
+const { validarJWT } = require("../middlewares/validarJWT");
+const { check } = require("express-validator");
+const { fieldValidator } = require("../middlewares/fieldValidator");
+const { isDate } = require("../helpers/isDate");
+
+const router = Router();
+
+router.use(validarJWT);
 
 //* Get events
-router.get("/", getEventos);
+router.get("/", getEvents);
 
 //* Crear event
-router.post("/", createEvent);
+router.post(
+  "/",
+  [
+    check("title", "El titulo es obligatorio.").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatorio.").custom(isDate),
+    check("end", "Fecha de finalizacion es obligatorio.").custom(isDate),
+
+    fieldValidator,
+  ],
+  createEvent
+);
 
 //* update event
 router.put("/:id", updateEvent);
 
 //* delete event
 router.delete("/:id", deleteEvent);
+
+module.exports = router;
