5424b684af08bfe3f1f0ae7eba24428dcb0ce165
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-06-30 15:07:18 -0600

    create and get all

--- controllers/events.js
+++ controllers/events.js
@@ -1,19 +1,38 @@
 const { response } = require("express");
+const Evento = require("../models/Evento");
 
 const getEvents = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "getEventos",
-  });
+  try {
+    const eventos = await Evento.find().populate("user", "name");
+    res.json({
+      ok: true,
+      msg: eventos,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const createEvent = async (req, res = response) => {
-  console.log(req.body);
-
-  res.json({
-    ok: true,
-    msg: "createEvent",
-  });
+  const evento = new Evento(req.body);
+  try {
+    evento.user = req.uid;
+    const eventoSave = await evento.save();
+    res.json({
+      ok: true,
+      evento: eventoSave,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const updateEvent = async (req, res = response) => {
